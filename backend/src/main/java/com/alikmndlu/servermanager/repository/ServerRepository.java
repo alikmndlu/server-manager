47de48f15e420fa26e8b7c57c0a43488aa2896c1
Ali Kmndlu <alikmndlu1@gmail.com>
2021-12-15 22:46:26 +0330

    add server repository

---
+++ backend/src/main/java/com/alikmndlu/servermanager/repository/ServerRepository.java
@@ -0,0 +1,9 @@
+package com.alikmndlu.servermanager.repository;
+
+import com.alikmndlu.servermanager.model.Server;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServerRepository extends JpaRepository<Server, Long> {
+
+    Server findByIpAddress(String ipAddress);
+}
